203c8d539b468584578ba9d5cfaea1b6be6babae
Akayashuu <sauvageleo1@gmail.com>
2022-11-27 17:12:21 +0100

    Update config files

--- src/Config/ConfigDatabase.php
+++ src/Config/ConfigDatabase.php
@@ -3,17 +3,46 @@
 namespace App\Config;
 
 
+/**
+ * Class de configuration de base de données
+ * @Namespace App\Config
+ * 
+ * Fonctionnement de la classe :
+ * 
+ * Elle contiens tous les informations demandé par PDO pour se connecter au SGBDR
+ */
 class ConfigDatabase {
 
+    /**
+     * Nom de la base de données
+     */
     private $dbname = "aeroclub";
+    /**
+     * Host 
+     * (localhsot dans notre cas) sinon une IP
+     */
     private $host = 'localhost';
+    /**
+     * Login du compte de la bdd
+     */
     private $log = 'app';
+    /**
+     * Mot de passe du compte de la bdd
+     */
     private $mdp = 'ua95qI0eTN^Y8@99a8@a5pF3Tyw96';
+    /**
+     * Port de la bdd
+     */
     private $port = '5432';
 
-
+    /**
+     * Constructor
+     */
     public function __construct() {}
     
+    /**
+     * Getter magique
+     */
     public function __get($name) {
         return isset($this->$name) ? $this->$name : false;
     }
--- src/Config/ConfigWeb.php
+++ src/Config/ConfigWeb.php
@@ -3,12 +3,30 @@
 namespace App\Config;
 
 
+
+/**
+ * Class de configuration web
+ * @Namespace App\Config
+ * 
+ * Fonctionnement de la classe :
+ * 
+ * Elle contiens le chemin par défaut ou se trouve le site dans l'arbo
+ */
 class ConfigWeb {
 
+    /**
+     * Chemin par défault
+     */
     private $defaultDir="/aeroclub";
 
+    /**
+     * Constructor
+     */
     public function __construct() {}
     
+    /**
+     * Getter magique
+     */
     public function __get($name) {
         return isset($this->$name) ? $this->$name : false;
     }
